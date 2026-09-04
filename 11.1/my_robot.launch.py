9a4ec9b8cbd2365d28941ca7e4b250669393399e
wael26214-blip <wael26214@gmail.com>
2026-09-04 16:21:18 +0300

    Refactor launch file for Gazebo integration
    Updated launch file to use gazebo_ros package and spawn_entity.py.

--- 11.1/my_robot.launch.py
+++ 11.1/my_robot.launch.py
@@ -7,41 +7,41 @@ from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
+
+    
     pkg_path = os.path.expanduser('~/Task-11/11.1/src/my_robot_pkg')
     xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file).toxml()
 
+   
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': robot_description_config}]
     )
 
-    gz_sim_pkg = get_package_share_directory('ros_gz_sim')
+
+    
+    gazebo_ros_pkg = get_package_share_directory('gazebo_ros')
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(gz_sim_pkg, 'launch', 'gz_sim.launch.py')
-        ),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+            os.path.join(gazebo_ros_pkg, 'launch', 'gazebo.launch.py')
+        )
     )
 
+
+    
     spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=['-topic', 'robot_description', '-name', 'my_robot'],
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_robot'],
         output='screen'
     )
 
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'],
-        output='screen'
-    )
 
+    
     return LaunchDescription([
         gazebo_launch,
         robot_state_publisher_node,
         spawn_entity,
-        bridge,
     ])
